9cc4d2c7f7d1259e1942b3f64799a4231817aed0
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-06-12 17:40:39 +0300

    assignment completed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
-
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+/* Add NavLink to import */
+import { BrowserRouter, Route, NavLink, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import About from "./components/About";
+import Home from "./components/Home";
+import NavBar from "./components/Navbar";
+ReactDOM.render(
+  <BrowserRouter>
+    <NavBar />
+    <Switch>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
